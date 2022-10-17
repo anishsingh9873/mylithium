e24b70a15812ae916d0b9e5261dfec9de75c62e6
anishsingh <anishsingh9873@gmail.com>
2022-10-17 19:14:59 +0530

    Its a api

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,7 +16,7 @@ router.get('/students', function (req, res){
 
 
 // Example 1 for path params
-router.get('/students/:studentName', function(req, res){
+router.get('/students/:studentName/:batchName', function(req, res){
     // ':' denotes that the following part of route is a variable
     // The value of this variable is what we are sending in the request url after /students
     // This value is set in the form of an object inside req.params
@@ -28,16 +28,62 @@ router.get('/students/:studentName', function(req, res){
     // params attribute is fixed in a request object
     // params contains the path parameters object
     console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
+    res.send('The full name is ' + myParams.studentName + ' its a ' + myParams.batchName + ' student.')
 })
 
 // Example 2 for path params
-router.get('/student-details/:name', function(req, res){
+/*router.get('/student-details/:name', function(req, res){
     let requestParams = req.params
     console.log("This is the request ", requestParams)
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
+})*/
+
+
+/*Question 1*/
+router.get('/movies',function(req,res){
+    const movie=['RRR','KGF 2','PS 1']
+    res.send(movie)
 })
+
+/*Question 4*/
+router.get('/films',function(req,res){
+    const film=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+       res.send(film)
+})
+
+
+/*Question 5*/
+router.get('/films/:filmId',function(req,res){
+    let myFilm=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+    console.log('Its a: ',myFilm)
+    res.send('This is a ' + myFilm.filmId)
+})
+
 
 module.exports = router;
