e4044a5bbc3c800f08be6ca8e1411103459d35ad
anishsingh <anishsingh9873@gmail.com>
2022-10-18 13:18:35 +0530

    its a api

--- src/routes/route.js
+++ src/routes/route.js
@@ -29,26 +29,26 @@ router.get('/students/:studentName/:batchName', function(req, res){
     // params contains the path parameters object
     console.log("The path params in the request are : ", myParams)
     res.send('The full name is ' + myParams.studentName + ' its a ' + myParams.batchName + ' student.')
-})*/
+})
 
 // Example 2 for path params
-/*router.get('/student-details/:name', function(req, res){
+router.get('/student-details/:name', function(req, res){
     let requestParams = req.params
     console.log("This is the request ", requestParams)
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
-})*/
+})
 
 
 //Question 1
-/*router.get('/movies',function(req,res){
+router.get('/movies',function(req,res){
     const movie=['RRR','KGF 2','PS 1']
     res.send(movie)
-})*/
+})
 
 //Question 4
-/*router.get('/films',function(req,res){
+router.get('/films',function(req,res){
     const film=[ {
         'id': 1,
         'name': 'The Shining'
@@ -63,53 +63,66 @@ router.get('/students/:studentName/:batchName', function(req, res){
         'name': 'Finding Nemo'
        }]
        res.send(film)
-})*/
+})
 
 
 //Question 5
+/*let myFilm=[ {
+    'id': 1,
+    'name': 'The Shining'
+   }, {
+    'id': 2,
+    'name': 'Incendies'
+   }, {
+    'id': 3,
+    'name': 'Rang de Basanti'
+   }, {
+    'id': 4,
+    'name': 'Finding Nemo'
+   }]
 router.get('/films/:filmId',function(req,res){
-    const film=req.params
-    console.log(film)
-    let myFilm=[ {
-        'id': 1,
-        'name': 'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-    if (film.index>myFilm.length) {
-        res.send('Its a :')
+    const film=req.params.filmId
+    const m=myFilm.find(filmId=>filmId==film)
+    if(m){
+        res.send(m)
     }
-res.send(myFilm[film.index])
+    else{
+        res.send('No any movie')
+    }
 })
 
 //Question 2
-/*router.get('/movies/:index',function(req,res){
-    const inParama=req.params
-    console.log(inParama)
-    const movie=['KGF','KGF2','PUSHPA','PS1','RRR']
-    if (inParama.index>movie.length) {
-        res.send('Its a : ')
-    }
-    res.send(movie[inParama.index])
-})*/
+const moviem=['KGF','KGF2','PUSHPA','PS1','RRR']
+router.get('/movies/:indexNumber',function(req,res){
+    const index=req.params.indexNumber
+    
+    res.send(moviem[index])
+})
 
 //Question 3
-/*router.get('/movies/:index',function(req,res){
-    const inParama=req.params
-    console.log(inParama)
-    const movie=['KGF','KGF2','PUSHPA','PS1','RRR']
-    if (inParama.index>movie.length) {
+const movie = ['KGF', 'KGF2', 'PUSHPA', 'PS1', 'RRR']
+router.get('/movies/:indexNumber', function (req, res) {
+    const inParama = req.params.indexNumber
+
+    if (inParama > (movie.length - 1)) {
         res.send('Not valid')
     }
-    res.send(movie[inParama.index])
+    else {
+        res.send(movie[inParama])
+    }
 })*/
+router.get('/shoes',function(req,res){
+    let queryParams=req.query
+    let brand=queryParams.brand
+    let discount=queryParams.discount
+    let color=queryParams.color
+    console.log('This brand selected is ', brand)
+    console.log('This selected discount is ', discount)
+    console.log('This color selected is ', color)
+    res.send('Dummy')
+
+})
+
 
 
 module.exports = router;
